72c2d3cb18dac5ab50729538c3782dfe7c1fb64b
Chenyuan Xie <1147719157xie@gmail.com>
2016-07-19 21:54:52 -0400

    * Version : 1.0 -  *                  Added tally function  *                  Project 1 finished

--- Assignment_4/Program.cs
+++ Assignment_4/Program.cs
@@ -9,8 +9,9 @@ using System.Threading.Tasks;
  * Date : July 19th, 2016
  * Date Modified: Jul 19th, 2016
  * Description : Assignment 4 Project 1 Dice Roll
- * Version : 0.3 - 
- *                  Added Display and Sort function
+ * Version : 1.0 - 
+ *                  Added tally function
+ *                  Project 1 finished
  */
 namespace Assignment_4
 {
@@ -21,12 +22,14 @@ namespace Assignment_4
             Random random = new Random(); // pseudo random number object
             List<int> diceRolls = new List<int>();// List that hold the dice result
             int maxRoll = 36000;// roll the dice 36000 times
+            int[] tally = new int[13];// array tally hold value 0-13
             for (int i = 0; i < maxRoll; i++)
             {
                 int firstDice = random.Next(1, 7);// First Dice
                 int secondDice = random.Next(1, 7);// Second Dice
                 int dice = firstDice + secondDice;// First Dice Add Second Dice
                 diceRolls.Add(dice);// Add each dice roll result to the list dice roll
+                tally[dice]++;// every time we get a result add 1 to tally
             }
 
             diceRolls.Sort();
@@ -36,6 +39,11 @@ namespace Assignment_4
                 Console.Write(result + " ");
             }
             Console.WriteLine("\n");
+
+            for (int i = 2; i < 13; i++)// display the number of result for each number
+            {
+                Console.WriteLine("{0}: {1}   {2}", i, tally[i]);
+            }
 
         }
     }
