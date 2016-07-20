f63083f552305be6714b069949bc9efc3ba82f04
Chenyuan Xie <1147719157xie@gmail.com>
2016-07-19 20:57:41 -0400

    * Version : 0.1 -  *                  Initial commit  *                  Added random number object

--- Assignment_4/Program.cs
+++ Assignment_4/Program.cs
@@ -1,15 +1,26 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Author : Chenyuan Xie
+ * Date : July 19th, 2016
+ * Date Modified: Jul 19th, 2016
+ * Description : Assignment 4
+ * Version : 0.1 - 
+ *                  Initial commit 
+ *                  Added random number object
+ */
 namespace Assignment_4
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Random random = new Random(); // pseudo random number object
+
         }
     }
 }
